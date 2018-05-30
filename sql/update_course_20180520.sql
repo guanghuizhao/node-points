8a95456e956dcfb0b38039c99584b8a22919ac98
zhaoguanghui03 <zhaoguanghui03@baidu.com>
2018-05-31 00:59:24 +0800

    sql

--- sql/update_course_20180520.sql
+++ sql/update_course_20180520.sql
@@ -1,4 +1,4 @@
 -- 更改上课时间为开始时间和结束时间两个字段
 ALTER TABLE `node-points`.`courses`
 CHANGE COLUMN `time` `start_time` datetime(0) NOT NULL COMMENT '上课开始时间' AFTER `teather`,
-ADD COLUMN `end_time` datetime(0) NOT NULL COMMENT '上课结束时间' AFTER `start_time`
+ADD COLUMN `end_time` datetime(0) NOT NULL COMMENT '上课结束时间' AFTER `start_time`;
--- sql/update_student_20180520.sql
+++ sql/update_student_20180520.sql
@@ -1,1 +1,10 @@
-ALTER TABLE `node`.`students` ADD COLUMN `wechat_unionId` varchar(64) NOT NULL AFTER `updated_at`, ADD UNIQUE `unionid` USING HASH (`wechat_unionId`) comment '';
+ALTER TABLE `node-points`.`students`
+ADD COLUMN `openid` varchar(50) NULL COMMENT '微信用户id' AFTER `wallet_address`,
+ADD UNIQUE INDEX `openid_key`(`openid`(10)) USING HASH;
+
+ALTER TABLE `node-points`.`students`
+ADD COLUMN `profile` varchar(255) NULL COMMENT '头像URL' AFTER `openid`;
+
+ALTER TABLE `node-points`.`students`
+ADD COLUMN `comments` varchar(255) NULL COMMENT '备注，可以存放其他信息' AFTER `profile`;
+
